bc286818e8e8658d311170b400eb77845ec68636
Diego Cardoza Z <cardoza.diego@gmail.com>
2018-04-18 00:06:59 -0300

    update

--- Secante.m
+++ Secante.m
@@ -1,51 +1,45 @@
-% Metodo Falsa Posicion.
-%fplot(@(x) x.^3 - 4*sin(x) + 1), grid on
+% Metodo Secante.
 
 func = @(x) x.^3 - 4*sin(x) + 1;
 a = 1;
 b = 2;
 e = 1e-5;
 
-[raiz, vecRaiz, vecError, vecA, vecB, it, a_or, b_or] = false_position(func, a, b, e)
+[raiz, vecRaiz, vecError, it, a_or, b_or] = secante(func, a, b, e);
 
 varNames = {'Iteraciones', 'Raiz', 'Error'};
 [Table, latexT] = tableGenerator(it, [vecRaiz; vecError], varNames);
 disp(Table)
-disp(char(latexT))
+%disp(char(latexT))
 
-function [raiz, vecRaiz, vecError,  vecA, vecB, it, a_or, b_or] = false_position(f, a, b, e) 
+function [raiz, vecRaiz, vecError, it, a_or, b_or] = secante(f, a, b, e)
 error = 1;
 k = 1;
 a_or = a;
 b_or = b;
 
-if f(a)*f(b) < 0    
+if f(a)*f(b) < 0
     while e < error
         f_a = f(a);
         f_b = f(b);
-
+        
         a1_1 = a * f_b;
         a1_2 = b * f_a;
         a2_1 = f_b - f_a;
-
+        
         raiz = (a1_1 - a1_2) / (a2_1);
         vecRaiz(k+1) = raiz;
-        f_c = f(raiz);
         
+        f_c = f(raiz);
+        a = b;
+        b = raiz;
+
         error = abs((vecRaiz(k+1) - vecRaiz(k))/vecRaiz(k+1));
         vecError(k+1) = error;
-        vecA(k+1) = a;
-        vecB(k+1) = b;
-                
-        if f_a * f_c < 0
-            b = raiz;
-        elseif f_c*f_b < 0
-            a = raiz;
-        end %end if
-
+        
         k = k + 1;
-    end %end while
-end %end if
+    end
+end
 
 it = k - 1;
 vecRaiz = vecRaiz(2:end);
@@ -55,6 +49,10 @@ fplot(f, [a_or b_or]), grid on
 xlabel('x')
 ylabel('y')
 
-title = strcat('FalsaPosicion_[', num2str(a_or), ', ', num2str(b_or), ']');
+title = strcat('Secante[', num2str(a_or), ', ', num2str(b_or), ']');
 print(title, '-deps')
 end %end function
+
+
+
+
