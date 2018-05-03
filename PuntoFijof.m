d26c8d8f6b3a32382acf156568062ef2f8f55ebd
Joaquin Cancino <joaquin.canzalez@gmail.com>
2018-05-03 02:06:12 -0300

    Update PuntoFijof.m

--- PuntoFijof.m
+++ PuntoFijof.m
@@ -1,35 +1,37 @@
 %Metodo Punto fijo
-g= @(x) (6-x)/3;
-x0= 1;
-a = 1;
-b = 2;
-tol = 1e-4;
+syms x;
+g= x*cos(x)- exp(x)+1;
+x0= -0.5;
+tol = 1e-5;
 
-[raiz, vecX, vecError, itf] = punto_fijo(g, a, b, x0, tol);
+[raiz, vecX, vecError, itf] = punto_fijo(g, x0, tol);
 
 varNames = {'Iteraciones', 'Raiz', 'Error'};
 [Table, latexT] = tableGenerator(itf, [vecX; vecError], varNames);
 disp(Table)
 %disp(char(latexT))
 
-function [raiz, vecX, vecError, itf] = punto_fijo(g, a, b, x0, tol)
+function [raiz, vecX, vecError, itf] = punto_fijo(g, x0, tol)
+a = -1;
+b = 1;
 error = 1;
 it = 1;
-syms x;
-dg = diff(g, x);
 
-while tol < error && abs(eval(dg)) < 1
+tic; 
+
+while tol < error
     aux = x0;
-    x0 = g(aux);
+    x0 = eval(subs(g, aux));
 
     vecX(it + 1) = aux;
 
-    error = abs((vecX(it+1) - vecX(it)) / vecX(it+1));
+    error = abs(vecX(it+1) - vecX(it));
     vecError(it+1) = error;
 
     it = it + 1;
 end
 
+tiempo_cpu = toc;
 raiz = aux;
 itf = it - 1;
 vecX = vecX(2:end);
@@ -38,7 +40,3 @@ vecError = vecError(2:end);
 fplot(g, [a b]), grid on
 xlabel('x')
 ylabel('y')
-
-title = strcat('Punto Fijo[', num2str(a), ', ', num2str(b), ']');
-print(title, '-deps')
-end %end function
